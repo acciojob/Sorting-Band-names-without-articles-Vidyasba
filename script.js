83d4a11f298c566eddc19d9eb0d08a2d459c0536
Vidyasba <116746304+Vidyasba@users.noreply.github.com>
2023-08-17 15:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+function removeArticles(name) {
+    const articles = ['a', 'an', 'the'];
+    const words = name.toLowerCase().split(' ');
 
+    if (articles.includes(words[0])) {
+      words.shift();
+    }
+
+    return words.join(' ');
+  }
+/ Sort the band names while removing articles
+  touristSpots.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+  // Get the ul element by id
+  const bandList = document.getElementById('band');
+
+  // Populate the ul element with li elements
+  touristSpots.forEach(name => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    bandList.appendChild(li);
+  });
